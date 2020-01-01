af9fa70604efaa97d1d48e264006ff4962b27262
ayaz.relytree <ayaz.relytree@gmail.com>
2020-01-01 10:24:32 +0530

    Created socket.io connection and established the first client connection

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var express = require("express");
+var socket = require("socket.io");
 
 // App setup
-
 var app = express();
 var server = app.listen(4000, function(){
     console.log("Listening to request on port 4000");
 });
 
 // Static files
-
 app.use(express.static("public"));
+
+// Socket setup
+var io = socket(server);
+
+io.on("connection", function(socket) {
+    console.log("made socket connection", socket.id)
+});
