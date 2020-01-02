6c1fb257809b9357edc3d68fcb3eb9e42e58fa2d
ayaz.relytree <ayaz.relytree@gmail.com>
2020-01-02 08:49:50 +0530

    Created the basic functionality of chat application

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var socket = require("socket.io");
 
 // App setup
 var app = express();
-var server = app.listen(4000, function(){
+var server = app.listen(4000, function () {
     console.log("Listening to request on port 4000");
 });
 
@@ -13,6 +13,10 @@ app.use(express.static("public"));
 // Socket setup
 var io = socket(server);
 
-io.on("connection", function(socket) {
+io.on("connection", function (socket) {
     console.log("made socket connection", socket.id)
+
+    socket.on("chat", function (data) {
+        io.sockets.emit("chat", data);
+    });
 });
