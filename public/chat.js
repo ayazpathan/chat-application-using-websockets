4f7d8cb88b3e25f6c9300044abb98f1cce6fae00
ayaz.relytree <ayaz.relytree@gmail.com>
2020-01-08 08:20:00 +0530

    Broadcasted Typing... message to all users

--- public/chat.js
+++ public/chat.js
@@ -26,3 +26,7 @@ socket.on("chat", function (data) {
     feedback.innerHTML = "";
     output.innerHTML += "<p><strong>" + data.handle + ":</strong>" + data.message + "</p>";
 });
+
+socket.on('typing', function (data) {
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+});
